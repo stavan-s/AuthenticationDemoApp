060db99b36870ccbd3040a29cba3d5104b53c29f
Admin <stavanshinde8@gmail.com>
2022-05-07 20:16:03 +0530

    handled back functionality

--- app/src/main/java/com/stavan/authenticationdemo/Login.java
+++ app/src/main/java/com/stavan/authenticationdemo/Login.java
@@ -62,7 +62,6 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Login.this, ResetPass.class));
-                finish();
             }
         });
 
